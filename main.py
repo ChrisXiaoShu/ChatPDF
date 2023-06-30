7adb693fe1d9ae2785a87cf8458fefef366d20b6
ChrisXiao <chris.xiao@hardcoretech.co>
2023-06-30 15:36:19 +0800

    feat(load-file-function): function for load pdf or txt file

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from langchain.document_loaders import TextLoader, PyPDFLoader
+
 load_dotenv()
+
+# function to load only .txt or .pdf file and will return error when file not found or can't not load
+def load_file(file_path) -> TextLoader or PyPDFLoader:
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"File is not a file: {file_path}")
+    if file_path.endswith(".txt") or file_path.endswith(".pdf"):
+        return PyPDFLoader(file_path) or TextLoader(file_path)
+    raise ValueError(f"File type not supported: {file_path}")
