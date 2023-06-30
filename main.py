72febb403cdc7d1fb186ddf72b07eac15d6b31d4
ChrisXiao <chris.xiao@hardcoretech.co>
2023-06-30 17:44:51 +0800

    fix(load-file-error): fix load file error
    1. logic error
    2. params error

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from langchain.document_loaders import TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
 
 load_dotenv()
 
@@ -14,13 +16,15 @@ def load_file(file_path) -> TextLoader or PyPDFLoader:
         raise FileNotFoundError(f"File not found: {file_path}")
     if not os.path.isfile(file_path):
         raise FileNotFoundError(f"File is not a file: {file_path}")
-    if file_path.endswith(".txt") or file_path.endswith(".pdf"):
-        return PyPDFLoader(file_path) or TextLoader(file_path)
+    if file_path.endswith(".txt"):
+        return TextLoader(file_path)
+    if file_path.endswith(".pdf"):
+        return PyPDFLoader(file_path)
     raise ValueError(f"File type not supported: {file_path}")
 
 # function pass Loadder and return splitter texts
 def split_text(loader: TextLoader or PyPDFLoader) -> list:
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunt_overlap=100)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     return loader.load_and_split(splitter)
 
 # create emmbeding vectorstore by using opentAIEmbedding model from given text
