f771e21449d3e28cb70ba93153b95d3806f69d38
ChrisXiaoShu <gah8974@gmail.com>
2023-07-15 14:36:19 +0800

    feat(basic): add basic version of recommand drink AI

---
+++ ai_model.py
@@ -0,0 +1,119 @@
+# create basic AI model class with template model variable and has get_chain and run methods
+from itertools import chain
+import re
+from typing import Any
+from click import prompt
+from langchain import ConversationChain, LLMChain, PromptTemplate
+from langchain.base_language import BaseLanguageModel
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+
+
+class AIModel:
+    """Base class for AI models."""
+
+    template: str
+    prompt: PromptTemplate
+    model: BaseLanguageModel
+
+    def get_chain(self, **kwargs: Any) -> Any:
+        raise NotImplementedError("get_chain not implemented")
+
+    def run(self, **kwargs: Any) -> Any:
+        """Run the model."""
+        chain = self.get_chain(**kwargs)
+        return chain.run(**kwargs)
+    
+class BartenderAI(AIModel):
+    model = ChatOpenAI(temperature=0)
+
+    template = """The following is a friendly conversation between a human and an AI. The AI is a professional bartender and help human find a cocktail that suits. AI should guide human in choosing a cocktail that is tailored to its preferences. AI should understand human preferences based on human preferred texture, type of alcohol, taste, or personal characteristics. please don't recommend a particular cocktail to human. AI job is merely understand human preference. And don't ask too complex question make question simple and one at a time. 請用繁體中文與我對答案。 
+    Current conversation:
+    {history}
+    Human: {input}
+    AI:
+    """
+
+    prompt = PromptTemplate(template=template, input_variables=["history", "input"])
+    
+    def get_chain(self, **kwargs: Any) -> Any:
+        return ConversationChain(
+            prompt=self.prompt,
+            llm=self.model,
+            memory=ConversationBufferMemory()
+        )
+            
+class SummaryPreferenceAI(AIModel):
+    model = ChatOpenAI(temperature=0)
+    template = """please summary the human preference from the following conversation in 繁體中文
+    Current conversation:
+    {history}
+    """
+    
+    prompt = PromptTemplate(template=template, input_variables=["history"])
+    
+    def get_chain(self, **kwargs: Any) -> Any:
+        return LLMChain(llm=self.model, prompt=self.prompt)
+    
+    def run(self, conversation_history):
+        chain = self.get_chain()
+        return chain.run({ 'history': conversation_history})
+
+
+class RecommandAI(AIModel):
+    model = ChatOpenAI(temperature=0)
+    template = """
+    please choice one of the cocktail from the menu below base on the human preference
+    here is human preference:
+    {preferences}
+    
+    here is the menu:
+    1. 青雲閤＄400
+    基底：gin琴酒
+    Nordes gin諾帝斯琴酒/St.germain接骨木花利口酒/Skinos乳香利口酒/
+    Jasmine Syrup自製茉莉花糖漿/Citrus Acid檸檬酸液/tonic通寧水
+    酒感2
+    口感：微甜/花香味強烈/清爽/氣泡感
+
+    2. 和泉樓＄400
+    基底：Gin琴酒
+    Generous Gin Azur大方琴酒/Crème de Violet 紫羅蘭利口酒/Lime Juice萊姆汁 /Lavender Syrup自製薰衣草糖漿/ La Caravedo Pisco秘魯白蘭地
+    酒感3.5
+    口感：偏酸爽口/如同香水的強烈花香
+
+    3. 醉花園＄450
+    基底：Homemade Rose Liqueur自製玫瑰利口酒
+    Homemade Rose Liqueur自製玫瑰利口酒/Red Repper Syrup粉紅胡椒糖漿/Hendricks Flora Adora Gin亨利爵士花神/Latic Acid乳酸/Cream鮮奶油/
+    Egg White蛋白/Soda Water蘇打水
+    酒感3
+    口感： 蛋糕般的綿密奶泡/主體玫瑰花香帶一絲粉紅胡椒的偏甜辛香
+
+    4. 鐵觀音＄400
+    基底：vodka伏特加
+    Tieguanyin tea infused vodka鐵觀音伏特加/Cointreau君度橙酒/
+    Crème de peach水蜜桃利口酒
+    酒感2
+    口感：水蜜桃甜香為前調/中調展現鐵觀音培茶風味/清爽的氣泡/酒體輕盈
+
+    5. 文山包種＄400
+    基底：Gin琴酒
+    Wen Shan Pochong包種茶琴酒/Pavan葡萄利口酒/Lavender Leaf Syrup自製薰衣草片糖漿/Lemon juice檸檬汁
+    酒感3
+    口感：偏甜爽口/花草香/麝香葡萄與橙花氣味為中調/茶香做為後韻
+
+    6. 金萱＄430
+    基底：White Wine白葡萄酒
+    Jin Xuan Tea Infused White Wine金萱茶白葡萄酒/Pineapple Sage Infused Apple Whiskey鳳梨鼠尾草蘋果威士忌/Chamomile Cordial洋甘菊風味液/Cream cheese Foam奶油起司泡沫
+    酒感3
+    口感：上層奶泡起司蛋糕風味呼應金萱茶獨特奶香/中調強烈洋甘菊轉為鼠尾草與蘋果的清新/微苦茶感與葡萄弱酸做結尾
+    
+    """
+    
+    prompt = PromptTemplate(template=template, input_variables=["preferences"])
+    
+    def get_chain(self, **kwargs: Any) -> Any:
+        return LLMChain(llm=self.model, prompt=self.prompt)
+    
+    def run(self, preferences):
+        chain = self.get_chain()
+        return chain.run({ 'preferences': preferences})
--- app.py
+++ app.py
@@ -1,15 +1,60 @@
-from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
-from langchain import PromptTemplate, LLMChain
+from sympy import O
+from ai_model import BartenderAI, RecommandAI, SummaryPreferenceAI
 import chainlit as cl
 
-template = """Question: {question}
-
-Answer: Let's think step by step."""
+from langchain import ConversationChain, LLMChain, PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory
 
 @cl.langchain_factory(use_async=True)
 def factory():
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=ChatOpenAI(temperature=0), verbose=True)
+    # Save the conversation history in the user session
+    cl.user_session.set("history", "")
+    cl.user_session.set("summary", "")
+    bartender = BartenderAI()
 
-    return llm_chain
+    return bartender.get_chain()
+
+@cl.action_callback("Summary Preference!")
+async def on_action(action):
+    history = cl.user_session.get("history")
+    summary_ai = SummaryPreferenceAI()
+    summary = summary_ai.run(history)
+    
+    cl.user_session.set("summary", summary)
+    
+    # Optionally remove the action button from the chatbot user interface
+    await action.remove()
+    
+    await cl.Message(content=summary).send()
+
+@cl.action_callback("Recommand Drink!")
+async def on_action(action):
+    summary = cl.user_session.get("summary")
+    if not summary:
+        history = cl.user_session.get("history")
+        summary_ai = SummaryPreferenceAI()
+        summary = summary_ai.run(history)
+    
+    recommand_ai = RecommandAI()
+    recommand = recommand_ai.run(summary)
+    
+    # Optionally remove the action button from the chatbot user interface
+    await action.remove()
+    
+    await cl.Message(content=recommand).send()
+
+@cl.langchain_postprocess
+async def postprocess(output: str):
+    # Sending an action button within a chatbot message
+    actions = [
+        cl.Action(name="Summary Preference!", value="example_value", description="Click me!"),
+        cl.Action(name="Recommand Drink!", value="example_value1", description="Click me1!"),
+    ]
+    cl.user_session.set("history", output['history'])
+    await cl.Message(content=output['response'], actions=actions).send()
